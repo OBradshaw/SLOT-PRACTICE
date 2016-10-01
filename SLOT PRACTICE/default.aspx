e87c5bd878b624d1e3bd9da503a3888038bc696e
Onisha <Onisha@HARLEYQUINN>
2016-09-30 19:26:44 -0500

    Add project files.

---
+++ SLOT PRACTICE/default.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="SLOT_PRACTICE._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            color: #FFFFFF;
+        }
+        .auto-style2 {
+            color: #FFFFFF;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .auto-style3 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        </style>
+</head>
+<body style="text-align: center; background:url('cast_banner_lost_girl_s5.jpg')">
+    <form id="form1" runat="server">
+    <div style="color: #FFFFFF">
+    
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    
+        <asp:Image ID="Image1" runat="server" BorderColor="Maroon" BorderStyle="Solid" BorderWidth="10px" Height="150px" Width="150px" BackColor="White" />
+        <asp:Image ID="Image2" runat="server" BorderColor="Maroon" BorderStyle="Solid" BorderWidth="10px" Height="150px" Width="150px" BackColor="White" />
+        <asp:Image ID="Image3" runat="server" BorderColor="Maroon" BorderStyle="Solid" BorderWidth="10px" Height="150px" Width="150px" BackColor="White" />
+        <br />
+        <br />
+        <br />
+        <span class="auto-style3"><strong>Your Bet:</strong></span>&nbsp;
+        <asp:TextBox ID="txtBet" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
+        <br />
+        <asp:Button ID="leverButton" runat="server" Text="Pull Lever!!!" OnClick="leverButton_Click" />
+        <br />
+        <h2>
+        <br class="auto-style1" />
+            <strong>
+        <asp:Label ID="cashLabel1" runat="server" CssClass="auto-style2"></asp:Label>
+            </strong></h2>
+        <h2>
+            <asp:Label ID="resultLabel1" runat="server"></asp:Label>
+            <strong>
+        <br class="auto-style2" />
+            </strong></h2>
+        <h2>
+            &nbsp;</h2>
+        <h2>
+            <span class="auto-style2"><strong>Rules:<br />
+        1 Cherry - x2 Your Bet</strong></span><strong><br class="auto-style2" />
+            </strong><span class="auto-style2"><strong>2 Cherries - x3 Your Bet</strong></span><strong><br class="auto-style2" />
+            </strong><span class="auto-style2"><strong>3 Cherries - x4 Your Bet</strong></span><strong><br class="auto-style2" />
+        <br class="auto-style2" />
+            </strong><span class="auto-style2"><strong>3 7&#39;s - Jackpot - x100 Your Bet</strong></span><strong><br class="auto-style2" />
+        <br class="auto-style2" />
+            </strong><span class="auto-style2"><strong>HOWEVER</strong></span><strong><br class="auto-style2" />
+        <br class="auto-style2" />
+            </strong><span class="auto-style2"><strong>If there&#39;s even one Bar you win nothing.
+    
+    </strong></span></h2>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ SLOT PRACTICE/default.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SLOT_PRACTICE
+{
+    public partial class _default : System.Web.UI.Page
+    {
+  
+            String[] imageNames;
+            Random random = new Random();
+            int Bet;
+            int Cash;
+            String slot1;
+            String slot2;
+            String slot3;
+
+            protected void setImages()
+            {
+                slot1 = imageNames[random.Next(11)];
+                slot2 = imageNames[random.Next(11)];
+                slot3 = imageNames[random.Next(11)];
+                Image1.ImageUrl = "/Images/" + slot1 + ".png";
+                Image2.ImageUrl = "/Images/" + slot2 + ".png";
+                Image3.ImageUrl = "/Images/" + slot3 + ".png";
+            }
+
+            protected void showCash()
+            {
+                cashLabel1.Text = "Player's Money " + Cash;
+            }
+
+            public bool hasBar()
+            {
+                if (slot1 == "Bar" || slot2 == "Bar" || slot3 == "Bar")
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            protected void showWin(int multiplier)
+            {
+                int winnings = Bet * multiplier;
+                Cash = Cash + winnings;
+                resultLabel1.Text = "You bet " + Bet + " and won " + winnings;
+            }
+
+            protected void showLoss()
+            {
+                resultLabel1.Text = "You Lost " + Bet;
+            }
+
+            public int checkWinMultiplier()
+            {
+                if (slot1 == "Seven" && slot2 == "Seven" && slot3 == "Seven")
+                    return 100;
+                int cherries = 0 + (slot1 == "Cherry" ? 1 : 0) + (slot2 == "Cherry" ? 1 : 0) + (slot3 == "Cherry" ? 1 : 0);
+                return cherries > 0 ? cherries + 1 : 0;
+            }
+
+            protected void calculateSpin()
+            {
+                Bet = int.Parse(txtBet.Text);
+                Cash -= Bet;
+                if (!hasBar() && checkWinMultiplier() > 0)
+                    showWin(checkWinMultiplier());
+                else
+                    showLoss();
+            }
+
+            protected void Page_Load(object sender, EventArgs e)
+            {
+                if (!Page.IsPostBack)
+                    ViewState.Add("Cash", 100);
+                Cash = Convert.ToInt32(ViewState["Cash"]);
+                imageNames = new String[12] { "Strawberry", "Bar", "Lemon", "Bell", "Clover", "Cherry", "Diamond", "Orange", "Seven", "HorseShoe", "Plum", "Watermelon" };
+                setImages();
+                showCash();
+            }
+
+            protected void leverButton_Click(object sender, EventArgs e)
+            {
+                setImages();
+                calculateSpin();
+                showCash();
+                ViewState["Cash"] = Cash;
+            }
+
+
+
+        }
+}
